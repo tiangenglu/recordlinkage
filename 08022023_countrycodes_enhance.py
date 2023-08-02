5ef75afb7345fca1a54580a2e9025b73bcccb4fe
Tiangeng Lu <58899759+tiangenglu@users.noreply.github.com>
2023-08-01 23:10:04 -0400

    reshape/edit country code dictionary to improve linkage

---
+++ 08022023_countrycodes_enhance.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 21:16:15 2023
+
+@author: Tiangeng Lu
+
+1. Scraped from https://www.census.gov/foreign-trade/schedules/c/country.txt
+2. Manipulation of dictionary/reference data to enhance recordlinkage performance
+3. Split rows with alternative country names and explode them to new rows
+4. Then, remove the original rows whose country names contains alternative names in "()".
+5. Keeping country names short has the following advantages: 
+    (a) Avoids mistakenly reducing `tfidf` token weights, and 
+    (b) Increases matching chances in `thefuzz`
+"""
+
+import pandas as pd
+import re
+codes_raw = pd.read_csv('country.txt', delimiter='\t', skiprows= [0, 1, 2, 3, 4, 245, 246, 247, 248], names = ['V'])
+
+#### BASIC CLEANING ####
+codes = codes_raw['V'].str.split('|', expand = True).rename(columns = {
+    0:'code',
+    1:'name',
+    2:'iso'})
+# applies to all columns
+codes = codes.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+codes['name'] = codes['name'].str.upper()
+del codes_raw
+
+#### EXTRACTING USEFUL INFO ####
+
+# DON'T ESCAPE
+alt_names = [row.split('(')[1].replace(')','') for row in codes['name'] if '(' in row]
+# DO ESCAPE
+main_names = [re.sub(r'\([^()]*\)', '', row).strip() for row in codes['name'] if '(' in row]
+# ISO and 4-digit code values
+codes_value = codes['code'][codes['name'].str.contains('\(')].tolist()
+iso_value = codes['iso'][codes['name'].str.contains('\(')].tolist()
+
+#### Dataframe ####
+df1 = pd.DataFrame(data = {
+    'code': codes_value,
+    'name': main_names,
+    'iso': iso_value})
+df2 = pd.DataFrame(data = {
+    'code': codes_value,
+    'name': alt_names,
+    'iso': iso_value})
+# discretionary removal for not-so-useful info
+df2 = df2[df2['name'] != 'IN THE INDIAN OCEAN']
+# concatenate all dataframes
+ScheduleC = pd.concat([codes,df1,df2]).\
+    sort_values(['code','name','iso'], ascending = True).\
+        drop_duplicates().reset_index(drop = True)
+ScheduleC = ScheduleC[~ScheduleC['name'].str.contains('\(')]
+del alt_names,main_names,codes_value,iso_value,df1,df2
+#### OUTPUT ####
+ScheduleC.to_csv('schedulec.csv', index = False)
